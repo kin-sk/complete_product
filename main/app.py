23f88b9d80d241b17c54ac43726d11915233a14f
kin-sk <kumikomise08@gamil.com>
2025-01-30 21:25:17 +0900

    ブランチ切られていたので修正しましたー

--- main/app.py
+++ main/app.py
@@ -1,9 +1,6 @@
 print("アクセス出来たら、下にprint関数でコメントをお願いします！")
 print("クソコメ1")
 print("ポリテク")
-<<<<<<< HEAD
 print("jajaja")
-=======
 print("ハロー世界")
->>>>>>> 3823b4e6d6e4c02acf8e933e8fe2c0ced51a9ca7
 print("ryoryo")
